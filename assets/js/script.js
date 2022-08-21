3cc2a12047aee57d8d29fcbd150b4e715556bbad
dsouths <dsouths@hotmail.com>
2022-08-21 18:51:00 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectScore() {
+
+}
+
+function incrementScore() {
+
+}
+
+function displayAdditionQuestion() {
+
+} 
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQueston() {
+
+}
+
+function displayDivideQuestion() {
+    
+}
